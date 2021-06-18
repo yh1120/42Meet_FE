d980a20c1b6de9acf98a045651faec0d379df474
yonglee <yonglee@student.42seoul.kr>
2021-06-18 15:39:22 +0900

    [FIX] #21 All states moved to Reservation Component
    Modal에 필요한 모든 state를 부모 Component로 옮기고 Modal에 적용했다.
    ModalInput의 내용을 Modal로 이동했다.

--- 42meet/src/Components/Modal/Modal.js
+++ 42meet/src/Components/Modal/Modal.js
@@ -1,40 +1,89 @@
-import React, { useState } from "react";
-import "./Modal.css";
+import React, { useState } from 'react';
+import './Modal.css';
 
 const Modal = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  const { open, close, header } = props;
+  const {
+    open,
+    close,
+    header,
+    selectedDate,
+    selectedRoom,
+    startTime,
+    endTime,
+    department,
+    title,
+    purpose,
+    members,
+  } = props;
   const [validate, setValidate] = useState(true);
   const submit = () => {
     //axios
     if (validate === true) {
-      window.location.href = "/mypage";
+      window.location.href = '/mypage';
     } else {
       close();
     }
   };
+  const infos = [
+    '예약 날짜',
+    '예약 장소',
+    '예약 시간',
+    '소속',
+    '행사명',
+    '사용목적',
+    '팀원',
+  ];
+  const values = {
+    [infos[0]]: selectedDate,
+    [infos[1]]: selectedRoom,
+    [infos[2]]: startTime + '시 ~ ' + endTime + '시',
+    [infos[3]]: department,
+    [infos[4]]: title,
+    [infos[5]]: purpose,
+    [infos[6]]: members ? (
+      <ul>
+        {members.map((member, idx) => {
+          return <li>{member}</li>;
+        })}
+      </ul>
+    ) : (
+      '없음'
+    ),
+  };
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className={open ? "openModal modal" : "modal"}>
+    <div className={open ? 'openModal modal' : 'modal'}>
       {open ? (
         <section>
           <header>
             {header}
             <button className="close" onClick={close}>
-              {" "}
-              &times;{" "}
+              {' '}
+              &times;{' '}
             </button>
           </header>
-          <main>{props.children}</main>
+          <main>
+            <div>
+              {infos.map((text, idx) => {
+                return (
+                  <div>
+                    <div key={text}>{text}</div>
+                    <div>{values[text]}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </main>
           <footer>
             <button className="submit" onClick={submit}>
-              {" "}
-              submit{" "}
+              {' '}
+              submit{' '}
             </button>
             <button className="close" onClick={close}>
-              {" "}
-              close{" "}
+              {' '}
+              close{' '}
             </button>
           </footer>
         </section>
