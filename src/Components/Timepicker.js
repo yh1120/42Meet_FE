92530b1d4a9da9f588f38181ea0ffcc2e522e20b
LeeJuhae <tlatmsdlwngo@naver.com>
2021-06-22 11:30:14 +0900

    [FIX] Add endtime

--- src/Components/Timepicker.js
+++ src/Components/Timepicker.js
@@ -5,12 +5,17 @@ const TimePicker = ({ name, reservedTime, userInput, setUserInput }) => {
   const timeArray = getHoursArray();
   const { selectedRoom } = userInput;
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     let selectedTime = e.target.value;
     if (name === 'startTime') {
       setUserInput({
         ...userInput,
-        startTime: selectedTime.slice(0, selectedTime.length - 2)
+        startTime: selectedTime.slice(0, selectedTime.length - 2),
+      });
+    } else {
+      setUserInput({
+        ...userInput,
+        endTime: selectedTime.slice(0, selectedTime.length - 2),
       });
     }
   };
