f0888171be63798e4c9e03016789b73c2e4760d9
yonglee <yonglee@student.42seoul.kr>
2021-06-29 17:08:11 +0900

    [FEAT] #76 view other reservations
    지난 예약과 예정된 예약을 클릭해서 볼 수 있도록 했다.
    ReservationList.js를 통해 자식 컴포넌트로 나눌 예정이다.
    
    지난 예약 혹은 현재 예약은 삭제하지 않고, 지난 예약을 일정 기간이 지나면
    삭제하도록 할 예정이다.

---
+++ src/Components/ReservationList.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import { getHeaders, setToken } from '../utils/utils';
+
+const ReservationList = ({ reservation, setValidate }) => {
+  const { date, startTime, endTime, roomName, members, leaderName, id } =
+    reservation;
+
+  const handleClick = (e) => {
+    const i = parseInt(e.target.id);
+    axios({
+      url: 'http://15.164.85.227:8081/delete',
+      method: 'POST',
+      headers: getHeaders(),
+      data: {
+        id: i,
+      },
+    }).then((response) => {
+      console.log('응답', response);
+      if (response.status === 200) {
+        setValidate(true);
+      }
+      setToken(response);
+    });
+  };
+
+  return (
+    <div
+      key={id}
+      style={{
+        width: '200px',
+        minHeight: '200px',
+        border: '1px solid black',
+        textAlign: 'center',
+        margin: '3px',
+        // alignContent: 'space-between',
+      }}
+    >
+      <div className="info">
+        <div className="tag">date</div>
+        <div>{date}</div>
+      </div>
+      <div className="info">
+        <div className="tag">time</div>
+        <div>
+          {`${parseInt(startTime.slice(0, 2))}시`} ~{' '}
+          {`${parseInt(endTime.slice(0, 2)) + 1}시`}
+        </div>
+      </div>
+      <div className="info">
+        <div className="tag">roomName</div>
+        <div> {roomName}</div>
+      </div>
+      <div className="info">
+        <div className="tag">leaderName</div>
+        <div>{leaderName}</div>
+      </div>
+      <div className="info">
+        <div className="tag">members </div>
+        <div
+          style={{
+            // display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+          }}
+        >
+          {members.map((member, m_idx) => {
+            return <div key={m_idx}>{member} </div>;
+          })}
+        </div>
+      </div>
+
+      <div>
+        <Button
+          id={id}
+          variant="dark"
+          disabled={
+            leaderName !== jwtDecode(localStorage.getItem('access-token')).sub
+              ? true
+              : false
+          }
+          onClick={handleClick}
+        >
+          &times;
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ReservationList;
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,10 +4,23 @@ import axios from 'axios';
 import { getHeaders, setToken } from '../utils/utils';
 import { Button } from 'react-bootstrap';
 import jwtDecode from 'jwt-decode';
+// import ReservationList from '../Components/ReservationList';
+import '../styles/MyPage.css';
 
 const MyPage = () => {
   const [myReservations, setMyReservations] = useState(null);
+  const [allReservations, setAllReservations] = useState(null);
   const [validate, setValidate] = useState(false);
+
+  const buttonColor = {
+    past: 'secondary',
+    present: 'secondary',
+    future: 'secondary',
+  };
+  const [colorForm, setColorForm] = useState({
+    ...buttonColor,
+    future: 'dark',
+  });
 
   const getReservations = async () => {
     try {
@@ -16,8 +29,10 @@ const MyPage = () => {
         method: 'GET',
         headers: getHeaders(),
       }).then((response) => {
+        setAllReservations(response.data);
         setMyReservations(response.data[1]);
         setToken(response);
+        console.log(response.data);
         // console.log('mypage', response.data[0]); // 현재 시간 예약, 오름차순
         // console.log('mypage', response.data[1]); // 예정 예약, 오름차순
         // console.log('mypage', response.data[2]); // 과거 예약, 내림차순
@@ -25,6 +40,24 @@ const MyPage = () => {
       setValidate(false);
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const handleList = (e) => {
+    const id = e.target.id;
+    setColorForm({
+      ...buttonColor,
+      [id]: 'dark',
+    });
+    if (id === 'past') {
+      console.log('past');
+      setMyReservations(allReservations[2]);
+    } else if (id === 'present') {
+      console.log('present');
+      setMyReservations(allReservations[0]);
+    } else {
+      console.log('future');
+      setMyReservations(allReservations[1]);
     }
   };
 
@@ -38,6 +71,7 @@ const MyPage = () => {
         id: i,
       },
     }).then((response) => {
+      console.log('응답', response);
       if (response.status === 200) {
         setValidate(true);
       }
@@ -45,73 +79,149 @@ const MyPage = () => {
     });
   };
 
-  // useEffect(() => {
-  //   if (myReservations === null || validate === true) {
-  //     getReservations();
-  //   }
-  // }, [myReservations, validate]);
-
   useEffect(() => {
-    if (myReservations === null) {
+    if (myReservations === null || validate === true) {
       getReservations();
     }
-  }, [myReservations]);
+  }, [myReservations, validate]);
+
+  //   useEffect(() => {
+  //     if (myReservations === null) {
+  //       getReservations();
+  //     }
+  //   }, [myReservations]);
 
   return (
     <div>
       <Navigation />
-      {myReservations !== null
-        ? Array.from(myReservations).map((reservation) => {
-            const {
-              date,
-              startTime,
-              endTime,
-              roomName,
-              participate,
-              leaderName,
-              id,
-            } = reservation;
-            return (
-              <div
-                key={id}
-                style={{
-                  border: '1px solid black',
-                  textAlign: 'center',
-                  margin: '3px',
-                }}
-              >
-                <div>date: {date}</div>
-                <div>
-                  time: {startTime} ~ {endTime}
+      {/* {reservationSeperate.map((seperate, idx) => {
+        console.log(typeof seperate, seperate);
+        return (
+          <div>
+            <div name={seperate}>{seperate}</div>
+            <ReservationList seperate={seperate} />
+          </div>
+        );
+      })} */}
+      <div
+        style={{
+          display: 'flex',
+          margin: '10px 10px 10px 10px',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Button
+          id="present"
+          variant={colorForm.present}
+          style={{ width: '115px' }}
+          onClick={handleList}
+        >
+          진행중인 예약
+        </Button>
+        <Button
+          id="future"
+          variant={colorForm.future}
+          style={{ width: '115px' }}
+          onClick={handleList}
+        >
+          다가올 예약
+        </Button>
+        <Button
+          id="past"
+          variant={colorForm.past}
+          style={{ width: '115px' }}
+          onClick={handleList}
+        >
+          지난 예약
+        </Button>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          minHeight: '300px',
+        }}
+      >
+        {myReservations !== null
+          ? Array.from(myReservations).map((reservation) => {
+              const {
+                date,
+                startTime,
+                endTime,
+                roomName,
+                members,
+                leaderName,
+                id,
+              } = reservation;
+              return (
+                <div
+                  key={id}
+                  style={{
+                    width: '200px',
+                    minHeight: '200px',
+                    border: '1px solid black',
+                    textAlign: 'center',
+                    margin: '3px',
+                    // alignContent: 'space-between',
+                  }}
+                >
+                  <div className="info">
+                    <div className="tag">date</div>
+                    <div>{date}</div>
+                  </div>
+                  <div className="info">
+                    <div className="tag">time</div>
+                    <div>
+                      {`${parseInt(startTime.slice(0, 2))}시`} ~{' '}
+                      {`${parseInt(endTime.slice(0, 2)) + 1}시`}
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="tag">roomName</div>
+                    <div> {roomName}</div>
+                  </div>
+                  <div className="info">
+                    <div className="tag">leaderName</div>
+                    <div>{leaderName}</div>
+                  </div>
+                  <div className="info">
+                    <div className="tag">members </div>
+                    <div
+                      style={{
+                        // display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      {members.map((member, m_idx) => {
+                        return <div key={m_idx}>{member} </div>;
+                      })}
+                    </div>
+                  </div>
+
+                  <div>
+                    <Button
+                      id={id}
+                      variant="dark"
+                      disabled={
+                        leaderName !==
+                        jwtDecode(localStorage.getItem('access-token')).sub
+                          ? true
+                          : false
+                      }
+                      onClick={handleClick}
+                    >
+                      &times;
+                    </Button>
+                  </div>
                 </div>
-                <div>roomName: {roomName}</div>
-                <div>leaderName: {leaderName}</div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  members: [
-                  {participate.map((member, m_idx) => {
-                    return <div key={m_idx}>{member} </div>;
-                  })}
-                  ]
-                </div>
-                <div>
-                  <Button
-                    id={id}
-                    variant="dark"
-                    disabled={
-                      leaderName !==
-                      jwtDecode(localStorage.getItem('access-token')).sub
-                        ? true
-                        : false
-                    }
-                    onClick={handleClick}
-                  >
-                    &times;
-                  </Button>
-                </div>
-              </div>
-            );
-          })
-        : null}
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
